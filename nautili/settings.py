fd00d0c507d5ceeb52471594ebc0db81800ac63c
aikikode <aikikode@gmail.com>
2014-06-02 15:50:08 +0400

    Use current monitor to display game window

--- nautili/settings.py
+++ nautili/settings.py
@@ -10,10 +10,22 @@ except pygame.error:  # pygame hadn't been initialized
     pygame.init()
     infoObject = pygame.display.Info()
 
+
+MONITOR_WIDTH = infoObject.current_w
+MONITOR_HEIGHT = infoObject.current_h
 win_delta = 100
-WIN_WIDTH = infoObject.current_w - win_delta
-WIN_HEIGHT = infoObject.current_h - win_delta
+try:
+    import gtk
+    window = gtk.Window()
+    screen = window.get_screen()
+    _, _, MONITOR_WIDTH, MONITOR_HEIGHT = screen.get_monitor_geometry(screen.get_monitor_at_window(screen.get_active_window()))
+    window.destroy()
+except ImportError:
+    pass
+WIN_WIDTH = MONITOR_WIDTH - win_delta
+WIN_HEIGHT = MONITOR_HEIGHT - win_delta
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
+
 MAIN_WIN_WIDTH = WIN_WIDTH
 MAIN_WIN_HEIGHT = WIN_HEIGHT
 RIGHT_PANEL_WIDTH = 200
